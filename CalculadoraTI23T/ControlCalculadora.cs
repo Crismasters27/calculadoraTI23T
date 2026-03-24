b71f0945763493579957aa72acca6b4c079bf612
CRISTIANO SILVERIO RAIMUNDO <cristiano.sraimundo@senacsp.edu.br>
2026-03-24 17:27:07 -0300

    Segundo COmmit

---
+++ CalculadoraTI23T/ControlCalculadora.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTI23T
+{
+    class ControlCalculadora
+    {
+        //Variáveis Globais
+        ModelCalculadora model;//Começando a conectar a control e a model
+
+        //Construtor vazio da classe ModelCalculadora
+        public ControlCalculadora()
+        {
+            this.model = new ModelCalculadora();//Efetivando a ligação com a Model
+        }//fim do construtor vazio
+
+        public ControlCalculadora(double num1, double num2)
+        {
+            this.model = new ModelCalculadora(num1, num2);
+        }//fim do construtor com parâmetros
+
+        //Método de Menu
+
+        public String mostrarMenu()
+        {
+            return "----------- Menu -------------\n\n" +
+                "Escolha uma das opções abaixo: \n" +
+                "0. Sair\n" +
+                "1. Somar\n" +
+                "2. Subtrair\n" +
+                "3. Multiplicar\n" +
+                "4. Dividir\n" +
+                "5. Potência\n" +
+                "6. Raiz Primeiro Número\n" +
+                "7. Raiz Segundo Número\n" +
+                "8. Tabuada\n" +
+                "9. Bissexto\n" +
+                "10. Verificar número entre 100 e 200\n" +
+                "11. Verificar se pode votar\n" +
+                "12. Verificar número em intervalo\n" +
+                "13. Top 10 maiores valores\n" +
+                "14. Verificar tipo de triângulo\n" +
+                "15. Informe o dia da semana\n"+
+                "16. Validar senha\n"+
+                "17. Comparar dois horários\n"+
+                "18. Mostrar o maior número"+
+                "19. Calcular média de 5 números\n";
+        }//fim do método
+
+        public void realizarOperacao()
+        {
+            int opcao = 0;//Declarar do lado de fora do do...while()
+            do
+            {
+                Console.WriteLine(this.mostrarMenu());//Chamar o menu
+                opcao = Convert.ToInt32(Console.ReadLine());//Leia
+                
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    case 1:
+                        this.coletar();//Pegar o que o usuário está digitando
+                        Console.WriteLine(this.model.somar());
+                        break;
+                    case 2:
+                        this.coletar();//Pegar o que o usuário está digitando
+                        Console.WriteLine("A subtração é: " + this.model.subtrair());
+                        break;
+                    case 3:
+                        this.coletar();//Pegar o que o usuário está digitando
+                        Console.WriteLine("A multiplicação é: " + this.model.multiplicar());
+                        break;
+                    case 4:
+                        this.coletar();//Pegar o que o usuário está digitando
+                        if (this.model.dividir() == -1)
+                        {
+                            Console.WriteLine("Impossível dividir por zero!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("A divisão é: " + this.model.dividir());
+                        }
+                        break;
+                    case 5:
+                        this.coletar();//Pegar o que o usuário está digitando
+                        Console.WriteLine("A potência é: " + this.model.potencia());
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe um número: ");
+                        this.model.setNum1(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("A raiz do primeiro número é: " + this.model.raizPrimeiroNumero());
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um número: ");
+                        this.model.setNum2(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("A raiz do segundo número é: " + this.model.raizSegundoNumero());
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe um número: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        //Para chamar a tabuada
+                        Console.WriteLine(this.model.tabuada(num));
+                        break;
+                    case 9:
+                        Console.WriteLine("Informe o ano: ");
+                        int ano = Convert.ToInt32(Console.ReadLine());
+                        //Chamar o método do bissexto
+                        Console.WriteLine(this.model.bissexto(ano));
+                        break;
+                    default:
+                        Console.WriteLine("Informe um valor correto!");
+                        break;
+                    case 10:
+                        Console.WriteLine("Informe um número: ");
+                        int numero = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.model.verificarIntervalo(numero));
+                        break;
+                    case 11:
+                        Console.WriteLine("Informe a idade: ");
+                        int idade = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.model.verificarVoto(idade));
+                        break;
+                    case 12:
+                        Console.WriteLine("Informe o número: ");
+                        double numeroIntervalo = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o início do intervalo: ");
+                        double inicio = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o fim do intervalo: ");
+                        double fim = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.model.verificarIntervaloPersonalizado(numeroIntervalo, inicio, fim));
+                        break;
+                    case 13:
+                        Console.WriteLine("Informe um número: ");
+                        double numeroTop = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.model.verificarTop10(numeroTop));
+                        break;
+                    case 14:
+                        Console.WriteLine("Informe o primeiro lado: ");
+                        double l1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo lado: ");
+                        double l2 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe o terceiro lado: ");
+                        double l3 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.model.verificarTriangulo(l1, l2, l3));
+                        break;
+                    case 15:
+                        Console.WriteLine("Informe um número de 1 a 7: ");
+                        int dia = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.model.diaDaSemana(dia));
+                        break;
+                    case 16:
+                        Console.WriteLine("Digite a senha: ");
+                        string senha = Console.ReadLine();
+
+                        Console.WriteLine(this.model.validarSenha(senha));
+                        break;
+                    case 17:
+                        Console.WriteLine("Informe o primeiro horário:");
+                        Console.Write("Horas (0–23): ");
+                        int h1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Minutos (0–59): ");
+                        int m1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo horário:");
+                        Console.Write("Horas (0–23): ");
+                        int h2 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Minutos (0–59): ");
+                        int m2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.model.horarioMaisTarde(h1, m1, h2, m2));
+                        break;
+                    case 18:
+                        Console.WriteLine("Digite o primeiro número: ");
+                        double n1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Digite o segundo número: ");
+                        double n2 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine($"O maior número é: {this.model.maiorNumero(n1, n2)}");
+                        break;
+                    case 19:
+                        double[] numeros = new double[5];
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.Write($"Digite o {i + 1}º número: ");
+                            numeros[i] = Convert.ToDouble(Console.ReadLine());
+                        }
+
+                        double media = this.model.calcularMedia(numeros);
+                        Console.WriteLine($"A média dos números é: {media}");
+                        break;
+                }//fim do escolha
+            } while (opcao != 0);
+        }//fim do método
+
+
+        public void coletar()
+        {
+            Console.Write("\n\nInforme o primeiro número: ");
+            this.model.setNum1(Convert.ToDouble(Console.ReadLine()));//Peguei o primeiro número
+
+            Console.Write("\n\nInforme o segundo número: ");
+            this.model.setNum2(Convert.ToDouble(Console.ReadLine()));//Peguei o segundo número
+        }//fim do coletar
+
+    }
+}
---
+++ CalculadoraTI23T/ModelCalculadora.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTI23T
+{
+    class ModelCalculadora
+    {
+        //1º Variáveis = GLOBALMENTE - Código inteiro enxerga elas
+        private double num1;//Encapsular a variável
+        private double num2;
+        private double resultado;
+        private List<double> top10 = new List<double>();
+
+        //Método que instancia as variáveis = dá valores iniciais
+        //2º Método construtor
+        public ModelCalculadora()
+        {
+            this.num1 = 0;
+            this.num2 = 0;
+            this.resultado = 0;
+        }//fim do consttrutor
+
+        public ModelCalculadora(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        
+        }//fim do construtor com parâmetros
+
+        //Método GET e SET
+        public double getNum1()
+        {
+            return this.num1;
+        }//fim do método get
+
+        public double getNum2()
+        {
+            return this.num2;
+        }//fim do get
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }//fim do get
+
+        //Métodos set - Modificadores
+        public void setNum1(double num1)
+        {
+            this.num1 = num1;
+        }//fim do set
+
+        public void setNum2(double num2)
+        {
+            this.num2 = num2;
+        }//fim do set
+
+        public void setResultado(double resultado)
+        {
+            this.resultado= resultado;
+        }//fim do set
+
+        //Operações aritméticas
+        public double somar()
+        {
+            setResultado(this.getNum1() + this.getNum2());//Alterando a resultado
+            return this.getResultado();//Mostrando a resultado
+        }//fim do método
+
+        public double subtrair()
+        {
+            this.setResultado(this.getNum1() - this.getNum2());
+            return this.getResultado();
+        }//fio do subtrair
+
+        public double multiplicar()
+        {
+            this.setResultado(this.getNum1() * this.getNum2());
+            return this.getResultado();
+        }//fim do multiplicar
+
+        public double dividir()
+        {
+            if (this.getNum2() == 0)
+            {
+                return -1;//Flag = Porque ele vai indicar que não da para dividir por zero
+            }
+            else
+            {
+                this.setResultado(this.getNum1() / this.getNum2());
+                return this.getResultado();
+            }
+        }//fim do dividir
+
+        //Potência
+        public double potencia()
+        {
+            this.setResultado(Math.Pow(this.getNum1(), this.getNum2()));
+            return this.getResultado();
+        }//fim do método
+
+        public double raizPrimeiroNumero()
+        {
+            this.setResultado(Math.Sqrt(this.getNum1()));
+            return this.getResultado();
+        }//fim do primeiro número
+
+        public double raizSegundoNumero()
+        {
+            this.setResultado(Math.Sqrt(this.getNum2()));
+            return this.getResultado();
+        }//fim do segundo número
+
+        public string tabuada(int num)
+        {
+            string msg = "";
+            int i = 0;
+            for (i = 0; i <= 10; i++)
+            {
+                msg += "\n" + num + " * " + i + " = " + (num * i);
+            }//fim do for
+            return msg;
+        }//fim do método
+
+        //Exercicio 1
+        public string bissexto(int ano)
+        {
+            if(ano % 4 == 0)
+            {
+                return $"{ano} é bissexto";//Interpolação
+            }
+            else
+            {
+                return $"{ano} NÃO é bissexto";
+            }
+        }//fim do bissexto
+
+        //Exercicio 2
+        public string verificarIntervalo(int num)
+        {
+            if (num >= 100 && num <= 200)
+            {
+                return $"{num} está entre 100 e 200";
+            }
+            else
+            {
+                return $"{num} NÃO está entre 100 e 200";
+            }
+        }
+
+        //Exercicio 3
+        public string verificarVoto(int idade)
+        {
+            if (idade < 16)
+            {
+                return "Não pode votar";
+            }
+            else if (idade >= 16 && idade < 18 || idade >= 70)
+            {
+                return "Voto facultativo";
+            }
+            else
+            {
+                return "Voto obrigatório";
+            }
+        }
+
+        //Exercicio 4
+        public string verificarIntervaloPersonalizado(double num, double inicio, double fim)
+        {
+            if (num >= inicio && num <= fim)
+            {
+                return $"{num} está dentro do intervalo de {inicio} até {fim}";
+            }
+            else
+            {
+                return $"{num} NÃO está dentro do intervalo de {inicio} até {fim}";
+            }
+        }
+
+        //Exercicio 5
+        public string verificarTop10(double num)
+        {
+            bool entrou = false;
+
+            if (top10.Count < 10)
+            {
+                top10.Add(num);
+                entrou = true;
+            }
+            else
+            {
+                double menor = top10.Min();
+
+                if (num > menor)
+                {
+                    top10.Remove(menor);
+                    top10.Add(num);
+                    entrou = true;
+                }
+            }
+
+            if (entrou)
+            {
+                return $"{num} está entre os 10 maiores valores";
+            }
+            else
+            {
+                return $"{num} NÃO está entre os 10 maiores valores";
+            }
+        }
+
+        //Exercicio 6
+        public string verificarTriangulo(double lado1, double lado2, double lado3)
+        {
+            // Primeiro verifica se os lados formam um triângulo
+            if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+            {
+                return "Lados inválidos";
+            }
+
+            if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1)
+            {
+                return "Não forma um triângulo";
+            }
+
+            // Agora verifica o tipo do triângulo
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                return "Triângulo Equilátero";
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                return "Triângulo Isósceles";
+            }
+            else
+            {
+                return "Triângulo Escaleno";
+            }
+        }
+
+        //Exercicio 7
+        public string diaDaSemana(int dia)
+        {
+            switch (dia)
+            {
+                case 1:
+                    return "Domingo";
+                case 2:
+                    return "Segunda-feira";
+                case 3:
+                    return "Terça-feira";
+                case 4:
+                    return "Quarta-feira";
+                case 5:
+                    return "Quinta-feira";
+                case 6:
+                    return "Sexta-feira";
+                case 7:
+                    return "Sábado";
+                default:
+                    return "Número inválido! Digite um valor entre 1 e 7.";
+            }
+        }
+
+        //Exercicio 8
+        public string validarSenha(string senha)
+        {
+            // Exemplo de regra de validação:
+            // - mínimo 6 caracteres
+            // - contém pelo menos 1 letra
+            // - contém pelo menos 1 número
+
+            if (senha.Length < 6)
+            {
+                return "Senha inválida: deve ter no mínimo 6 caracteres.";
+            }
+
+            bool temLetra = senha.Any(char.IsLetter);
+            bool temNumero = senha.Any(char.IsDigit);
+
+            if (!temLetra || !temNumero)
+            {
+                return "Senha inválida: deve conter letras e números.";
+            }
+
+            return "Senha válida!";
+        }
+
+        //Exercicio 9
+        public string horarioMaisTarde(int hora1, int minuto1, int hora2, int minuto2)
+        {
+            // Validação básica
+            if (hora1 < 0 || hora1 > 23 || hora2 < 0 || hora2 > 23 ||
+                minuto1 < 0 || minuto1 > 59 || minuto2 < 0 || minuto2 > 59)
+            {
+                return "Horário inválido! Use 0–23 para horas e 0–59 para minutos.";
+            }
+
+            // Comparar horários
+            if (hora1 > hora2 || (hora1 == hora2 && minuto1 > minuto2))
+            {
+                return $"O primeiro horário ({hora1:D2}:{minuto1:D2}) é mais tarde.";
+            }
+            else if (hora2 > hora1 || (hora2 == hora1 && minuto2 > minuto1))
+            {
+                return $"O segundo horário ({hora2:D2}:{minuto2:D2}) é mais tarde.";
+            }
+            else
+            {
+                return "Os dois horários são iguais.";
+            }
+        }
+
+        //Exercicio 10
+        public double maiorNumero(double num1, double num2)
+        {
+            if (num1 > num2)
+                return num1;
+            else
+                return num2;
+        }
+
+        //Exercicio 11
+        public double calcularMedia(double[] numeros)
+        {
+            double soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+
+            return soma / numeros.Length;
+        }
+
+    }//fim da classe Model
+}//fim do projetoCalculadoraTI23T
